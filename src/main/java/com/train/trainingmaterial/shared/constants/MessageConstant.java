c70b396ce41e9871596ddca8009ae0fd65f80035
Norris Nguyen <nguyennguyen@Norriss-MacBook-Pro.local>
2024-03-03 19:57:02 +0700

    feature/Create Handle Global Exception handler Controler

---
+++ src/main/java/com/train/trainingmaterial/shared/constants/MessageConstant.java
@@ -0,0 +1,29 @@
+package com.train.trainingmaterial.shared.constants;
+
+public class MessageConstant {
+  private MessageConstant() {}
+
+  // Authentication
+  public static final String AUTHENTICATION_ERROR = "Authentication error";
+  public static final String AUTHENTICATION_X_ERROR = "Authentication error %s";
+
+  // Error message response
+  public static final String VALIDATION_FAILED = "Validation failed";
+  public static final String USER_DOESNT_EXIST = "User doesn't exist";
+  public static final String USER_X_DOESNT_EXIST = "User %s doesn't exist";
+  public static final String TOKEN_IS_NOT_VALID = "Token is not valid";
+  public static final String TOKEN_IS_NOT_VALID_X = "Token is not valid %s";
+  public static final String INVALID_FILE_ERROR = "Invalid file error";
+  public static final String INVALID_FILE_X_ERROR = "Invalid file %s error";
+  public static final String DATA_PROCESSING_ERROR = "Data processing error";
+  public static final String DATA_PROCESSING_ERROR_X = "Data processing error %s";
+  public static final String TAG_EXISTED = "Tag existed";
+  public static final String TAG_X_EXISTED = "Tag %s existed";
+  public static final String TAG_NOT_EXISTED = "Tag not existed";
+  public static final String TAG_X_NOT_EXISTED = "Tag %s not existed";
+  public static final String CATEGORY_EXISTED = "Category existed";
+  public static final String CATEGORY_X_EXISTED = "Category %s existed";
+  public static final String CATEGORY_NOT_EXISTED = "Category not existed";
+  public static final String CATEGORY_X_NOT_EXISTED = "Category %s not existed";
+}
+
